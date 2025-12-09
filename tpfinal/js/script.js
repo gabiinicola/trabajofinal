b7b8375ce970a62d76b6ac788a437bdbcfacfa56
Gabriel Nicola <97078114+gabiinicola@users.noreply.github.com>
2025-12-09 17:37:05 -0300

    Add files via upload

--- tpfinal/js/script.js
+++ tpfinal/js/script.js
@@ -1,38 +1,53 @@
-
 (() => {
-    'use strict'
-    const forms = document.querySelectorAll('#contactForm')
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
+  'use strict';
 
+  const forms = document.querySelectorAll('#contactForm');
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
 
-  window.addEventListener("load", () => {
-    const overlay = document.querySelector(".overlay");
-    setTimeout(() => { overlay.style.display = "none"; }, 1500);
+window.addEventListener("load", () => {
+  const overlay = document.querySelector(".overlay");
 
-    const btn = document.getElementById("btnScroll");
-    btn.addEventListener("click", () => {
-        document.getElementById("catalogo").scrollIntoView({behavior: "smooth"});
-    });
+  setTimeout(() => {
+    overlay.style.display = "none";
+  }, 1500);
+
+  const btn = document.getElementById("btnScroll");
+
+  btn.addEventListener("click", () => {
+    document.getElementById("catalogo").scrollIntoView({ behavior: "smooth" });
+  });
 });
 
 window.addEventListener('load', () => {
-    setTimeout(() => {
-        const scrollDown = document.querySelector('.scroll-down');
-        scrollDown.classList.add('visible');
-    }, 3000);
+  setTimeout(() => {
+    const scrollDown = document.querySelector('.scroll-down');
+    scrollDown.classList.add('visible');
+  }, 3000);
 
-    const btnScroll = document.getElementById('btnScroll');
-    btnScroll.addEventListener('click', () => {
-        const catalogo = document.getElementById('catalogo');
-        catalogo.scrollIntoView({ behavior: 'smooth' });
-    });
+  const btnScroll = document.getElementById('btnScroll');
+
+  btnScroll.addEventListener('click', () => {
+    const catalogo = document.getElementById('catalogo');
+    catalogo.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+
+
+
+
+const catalogoCarousel = new bootstrap.Carousel('#catalogoCarousel', {
+  interval: 4000, 
+  ride: 'carousel',
+  pause: false,
+  wrap: true
 });
